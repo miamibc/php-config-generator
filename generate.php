1aa1dc0c9934d4edc71b2f7c2dd1ce5805ea5ff3
Sergei Miami <miami@blackcrystal.net>
2019-03-14 13:01:20 +0100

    Twig and Blade engines

--- generate.php
+++ generate.php
@@ -16,26 +16,41 @@ foreach ($argv as $i=>$item)
 {
   // skip first arguments
   if ($i < 2) continue;
-  // merge {json}
+
   elseif (substr($item, 0, 1) === '{' && substr($item,-1,1) === '}')
   {
     $vars = array_merge( $vars, json_decode( $item ,true));
   }
-  // merge file
+  elseif (preg_match('/^([^=]+)=(.*)$/', $item, $matches))
+  {
+    $vars[$matches[1]] = $matches[2];
+  }
   elseif (file_exists( $item ))
   {
     $vars = array_merge( $vars, json_decode( file_get_contents( $item ), true ));
   }
 }
 
-var_dump($vars);
+// var_dump($vars);
 
-echo("# Made with <3 using http://github.com/miamibc/php-config-generator\n");
-echo("# ".implode(" ", $argv)."\n\n");
+echo("# Made with turn http://github.com/miamibc/php-config-generator\n");
+echo("# ".implode(" ", $argv)."\n");
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/template');
-$twig = new \Twig\Environment($loader, [
-  'cache' => __DIR__ . '/cache',
-]);
-
-echo $twig->render( $argv[1], $vars);
+// render template
+if  (stripos($argv[1], '.twig') !== false )
+{
+  $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/template');
+  $twig = new \Twig\Environment($loader,[
+    'cache' => __DIR__.'/cache',
+  ]);
+  echo $twig->render( $argv[1], $vars );
+}
+if  ( ($pos = stripos($argv[1], '.blade')) !== false )
+{
+  $blade = new eftec\bladeone\BladeOne(
+    __DIR__ . '/template',
+    __DIR__ . '/cache',
+    eftec\bladeone\BladeOne::MODE_DEBUG
+  );
+  echo $blade->run( substr($argv[1], 0, $pos), $vars );
+}
